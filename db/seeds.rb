dd9d145decd30833c7e6eec5284b0324752a65b8
Patryk Pysiak <patrykpysiak@gmail.com>
2022-08-19 01:02:56 +0200

    lists and movies basic controllers and views done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,9 +7,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "seeding..."
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-puts "done"
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)
+
+data['results'].each { |item| puts item['title']}
+data['results'].each { |item| puts item['overview']}
+data['results'].each { |item| puts item['vote_average']}
+data['results'].each { |item| puts "https://image.tmdb.org/t/p/original#{item['poster_path']}"}
+data['results'].each do |item|
+  puts "seeding: #{item['title']}..."
+Movie.create(title: item['title'], overview: item['overview'], poster_url: "https://image.tmdb.org/t/p/original#{item['poster_path']}", rating: item['vote_average'])
+  puts "done"
+end
